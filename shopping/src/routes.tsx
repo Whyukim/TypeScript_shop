d531c75a6f21f42d56c2826790a9e8935a12c762
Whyukim <devhyukim@gmail.com>
2022-11-11 17:06:18 +0900

    first commit

---
+++ shopping/src/routes.tsx
@@ -0,0 +1,25 @@
+import React, { lazy } from "react";
+import GlobalLayout from "./pages/_layout";
+
+const DynamicIndex = lazy(() => import("./pages/index"));
+const DynamicProductIndex = lazy(() => import("./pages/products/index"));
+const DynamicId = lazy(() => import("./pages/products/[id]"));
+
+export const routes = [
+  {
+    path: "/",
+    element: <GlobalLayout />,
+    children: [
+      { path: "/", element: <DynamicIndex />, index: true },
+      { path: "/cart", element: <DynamicIndex />, index: true },
+      { path: "/products", element: <DynamicProductIndex />, index: true },
+      { path: "/products/:id", element: <DynamicId /> },
+    ],
+  },
+];
+
+export const pages = [
+  { route: "/" },
+  { route: "/products" },
+  { route: "/products/:id" },
+];
